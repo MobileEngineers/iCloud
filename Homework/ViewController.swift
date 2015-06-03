4ba9509fab6fab6a58a9896763ac5dc27de4402b
Isaías <isaiahlima18@gmail.com>
2015-06-03 08:45:19 -0300

    Singleton, projeto e demais bases programados

---
+++ Homework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Homework
+//
+//  Created by Isaías Lima on 02/06/15.
+//  Copyright (c) 2015 Mobile Engineers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
