dd1da0d3406d25e273f11ed92a717163b204549d
Matheus Amancio Seixeiro <matheusamancio23@hotmail.com>
2015-06-20 00:59:03 -0300

    ajustes

--- Homework/Calendario.swift
+++ Homework/Calendario.swift
@@ -32,16 +32,61 @@ class Calendario: UIViewController, UITableViewDelegate, UITableViewDataSource/*
         
         let calendar = NSCalendar.currentCalendar()
         let comps = NSDateComponents()
+        var comps2 = NSDateComponents()
         comps.day = 7
         let date2 = calendar.dateByAddingComponents(comps, toDate: NSDate(), options: NSCalendarOptions.allZeros)
+        
+        
+        let day = calendar.ordinalityOfUnit(.CalendarUnitDay, inUnit: .CalendarUnitMonth, forDate: atividade1.data)
+        println(day)
+        let Month = calendar.ordinalityOfUnit(.CalendarUnitMonth, inUnit: .CalendarUnitYear, forDate: atividade1.data)
+        println(Month)
+        let year = calendar.ordinalityOfUnit(.CalendarUnitYear, inUnit: .CalendarUnitEra, forDate: atividade1.data)
+        println(year)
+
+
         
         if atividade1.data.compare(date2!) == NSComparisonResult.OrderedDescending
         {
             NSLog("nao chegou em 7 dias");
         } else if atividade1.data.compare(date2!) == NSComparisonResult.OrderedAscending
         {
+            for(var i = 7; i>=0; i-- ){
+                comps2.day = i
+                println(comps2.day)
+                let date3 = calendar.dateByAddingComponents(comps2, toDate: NSDate(), options: NSCalendarOptions.allZeros)
+                
+                let day1 = calendar.ordinalityOfUnit(.CalendarUnitDay, inUnit: .CalendarUnitMonth, forDate: date3!)
+                println(day1)
+                let Month1 = calendar.ordinalityOfUnit(.CalendarUnitMonth, inUnit: .CalendarUnitYear, forDate: date3!)
+                println(Month1)
+                let year1 = calendar.ordinalityOfUnit(.CalendarUnitYear, inUnit: .CalendarUnitEra, forDate: date3!)
+                println(year1)
+                
+
+
+                
+                if(day1 == day && Month1 == Month && year1 == year){
+
+                println(comps2)
+                    if comps2.day == 6{
+                        array6.addObject(atividade1)}
+                    if comps2.day == 5{
+                        array5.addObject(atividade1)}
+                    if comps2.day == 4{
+                        array4.addObject(atividade1)}
+                    if comps2.day == 3{
+                        array3.addObject(atividade1)}
+                    if comps2.day == 2{
+                        array2.addObject(atividade1)}
+                    if comps2.day == 1{
+                        array1.addObject(atividade1)}
+                
+                }
+        
+        
             NSLog("esta dentro de sete dias");
-            array7.addObject(atividade1)
+            }
 
         } else
         {
@@ -58,7 +103,15 @@ class Calendario: UIViewController, UITableViewDelegate, UITableViewDataSource/*
         var request = NSFetchRequest(entityName: "Disciplina")
         request.returnsObjectsAsFaults = false
         
+        atividades.removeAllObjects()
         array7.removeAllObjects()
+        array6.removeAllObjects()
+        array5.removeAllObjects()
+        array4.removeAllObjects()
+        array3.removeAllObjects()
+        array2.removeAllObjects()
+        array1.removeAllObjects()
+
         
         disciplinas = CoreData.sharedInstance.managedObjectContext!.executeFetchRequest(request, error: nil)
         println(disciplinas.count)
@@ -83,6 +136,7 @@ class Calendario: UIViewController, UITableViewDelegate, UITableViewDataSource/*
                 atividade1.nome = trabalho1.nome
                 atividade1.data = trabalho1.data
                 
+                atividades.addObject(atividade1)
                 organizarEmDia(atividade1)
                 
             }
@@ -96,6 +150,8 @@ class Calendario: UIViewController, UITableViewDelegate, UITableViewDataSource/*
                 atividade1.tipo = "prova"
                 atividade1.nome = prova1.nome
                 atividade1.data = prova1.data
+                atividades.addObject(atividade1)
+
                 organizarEmDia(atividade1)
                 
             }
@@ -117,16 +173,65 @@ class Calendario: UIViewController, UITableViewDelegate, UITableViewDataSource/*
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return array7.count
-    
+        if section == 0{
+        return array1.count
+        }else
+        if section == 1{
+            return array2.count
+        }else
+        if section == 2{
+            return array3.count
+        }else
+        if section == 3{
+            return array4.count
+        }else
+        if section == 4{
+            return array5.count
+        }else
+        if section == 5{
+            return array6.count
+        }else {
+            return self.array7.count
+        }
     }
     
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 7
+    }
 
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
         var cell =  tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
-        var atividade1 = array7.objectAtIndex(indexPath.row) as! Atividade
-    cell.textLabel?.text = atividade1.nome
+        if indexPath.section == 0{
+            var atividade1 = array1.objectAtIndex(indexPath.row) as! Atividade
+            cell.textLabel?.text = atividade1.nome
+        }
+        if indexPath.section == 1{
+            var atividade1 = array2.objectAtIndex(indexPath.row) as! Atividade
+            cell.textLabel?.text = atividade1.nome
+        }
+        if indexPath.section == 2{
+            var atividade1 = array3.objectAtIndex(indexPath.row) as! Atividade
+            cell.textLabel?.text = atividade1.nome
+        }
+        if indexPath.section == 3{
+            var atividade1 = array4.objectAtIndex(indexPath.row) as! Atividade
+            cell.textLabel?.text = atividade1.nome
+        }
+        if indexPath.section == 4{
+            var atividade1 = array5.objectAtIndex(indexPath.row) as! Atividade
+            cell.textLabel?.text = atividade1.nome
+        }
+        if indexPath.section == 5{
+            var atividade1 = array6.objectAtIndex(indexPath.row) as! Atividade
+            cell.textLabel?.text = atividade1.nome
+        }
+        if indexPath.section == 6{
+            var atividade1 = array7.objectAtIndex(indexPath.row) as! Atividade
+            cell.textLabel?.text = atividade1.nome
+        }
+
+
     
         return cell
     }
